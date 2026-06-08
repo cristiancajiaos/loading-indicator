a25c00dba9870ea966927c83c05193acacb478f7
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-06-08 15:11:06 -0400

    Establecido layout de app por defecto

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoadingIndicator } from './components/loading-indicator/loading-indicator'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoadingIndicator],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
